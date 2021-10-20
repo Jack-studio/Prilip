ab412d003f4d00bb527afd9163e38ec6d956bd90
Jack-studio <71664690+Jack-studio@users.noreply.github.com>
2021-10-20 20:36:40 +0300

    Add files via upload

---
+++ hello/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'hello/index.html')    
+
+def yjen(request):
+    return HttpResponse('Hello, Yjen')
+
+def greet(request, name):
+ #   return HttpResponse(f'Hello, {name.capitalize()}!')       
+    return render(request, 'hello/greet.html',{
+        'name': name.capitalize()
+    })
